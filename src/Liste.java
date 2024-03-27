30f0faf05275a74ad06587ee589492f122f6fd2a
David G <david.giasson@claurendeau.qc.ca>
2024-03-27 10:27:14 -0400

    Mise à jour des méthodes retirer() pour une liste doublement chainée.

--- src/Liste.java
+++ src/Liste.java
@@ -126,15 +126,22 @@ public class Liste {
     }
 
     public boolean effacerAt(int index) {
-        if (index < 0 || index > nbElements)
+        if (index < -nbElements || index > nbElements)
             return false;
 
         if (index == 0) {
             premier = premier.prochain;
+            premier.precedent = null;
+        }
+        else if (index == nbElements - 1 || index == -1) {
+            dernier = dernier.precedent;
+            dernier.prochain = null;
         }
         else {
             Noeud precedent = getNoeudAt(index - 1);
-            precedent.prochain = precedent.prochain.prochain;
+            Noeud suivant = getNoeudAt(index + 1);
+            precedent.prochain = suivant;
+            suivant.precedent = precedent;
         }
 
         nbElements--;
